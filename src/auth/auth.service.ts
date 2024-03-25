b5c955e8f78bd42bbc64300dde90f8991a91316f
Yboda <ybr2265@naver.com>
2024-03-26 00:38:20 +0900

    feat: 로그인, 회원가입 기능 구현
    - auth entry 생성
    - login, register 로직 구현 및 엔드포인트 생성
    - bcrypt 적용

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,96 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersModel } from 'src/users/entities/users.entity';
+import { HASH_ROUNDS, JWT_SECRET } from './const/auth.const';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly usersService: UsersService,
+  ) {}
+  /**
+   * 1) registerWithEmail
+   *  - email, nickname, password를 입력받아 사용자 생성 -> access & refresh token 반환 (로그인까지)
+   *
+   * 2) loginWithEmail
+   *  - email, password 입력하면 사용자 검증 -> 검증완료시 access & refresh token 반환
+   *
+   * 공통함수)
+   * authWithEmailAndPassword : 이메일과 아이디 검증하는 로직
+   *  1. 사용자가 존재하는지 확인
+   *  2. 비밀번호 맞는지 확인
+   *  3. 1,2통과시 사용자정보 반환
+   *  4. 데이터를 기반으로 토큰 생성 및 반환 ↓
+   * signToken :  access & refresh token 만드는 로직
+   * loingUser :  access & refresh token 반환하는 로직
+   */
+
+  /**
+   * < payload에 들어갈 정보 >
+   * email, sub(id), type: access or refresh
+   */
+  signToken(user: Pick<UsersModel, 'email' | 'id'>, isRefreshToken: boolean) {
+    const payload = {
+      email: user.email,
+      sub: user.id,
+      type: isRefreshToken ? 'refresh' : 'access',
+    };
+
+    return this.jwtService.sign(payload, {
+      // @TODO: 추후 config 모듈 구성할 때 env 설정
+      secret: JWT_SECRET,
+      expiresIn: isRefreshToken ? 3600 : 300,
+    });
+  }
+
+  loginUser(user: Pick<UsersModel, 'email' | 'id'>) {
+    return {
+      accessToken: this.signToken(user, false),
+      refreshToken: this.signToken(user, true),
+    };
+  }
+
+  async authWithEmailAndPassword(user: Pick<UsersModel, 'email' | 'password'>) {
+    const existUser = await this.usersService.getUserByEmail(user.email);
+
+    if (!existUser) {
+      throw new UnauthorizedException('존재하지 않는 사용자입니다.');
+    }
+
+    /**
+     * 파라미터
+     *
+     * 1) 입력된 비밀번호
+     * 2) 기존 해쉬값 (저장되어있는 hash)
+     */
+    const isPass = await bcrypt.compare(user.password, existUser.password);
+
+    if (!isPass) {
+      throw new UnauthorizedException('비밀번호가 틀렸습니다.');
+    }
+
+    return existUser;
+  }
+
+  async loginWithEmail(user: Pick<UsersModel, 'email' | 'password'>) {
+    // 1. 유저 존재여부, 패스워드 체크
+    const existUser = await this.authWithEmailAndPassword(user);
+    // 2. access & refresh token 반환
+    return this.loginUser(existUser);
+  }
+
+  async registerWithEmail(
+    user: Pick<UsersModel, 'nickname' | 'email' | 'password'>,
+  ) {
+    const hash = await bcrypt.hash(user.password, HASH_ROUNDS);
+
+    const newUser = await this.usersService.createUser({
+      ...user,
+      password: hash,
+    });
+
+    return this.loginUser(newUser);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UsersModel } from './entities/users.entity';
@@ -14,15 +14,43 @@ export class UsersService {
     return this.usersRepository.find();
   }
 
-  async createUser(nickname: string, email: string, password: string) {
-    const user = this.usersRepository.create({
-      nickname,
-      email,
-      password,
+  async createUser(user: Pick<UsersModel, 'email' | 'nickname' | 'password'>) {
+    const emailExists = await this.usersRepository.exist({
+      where: {
+        email: user.email,
+      },
     });
 
-    const newUser = await this.usersRepository.save(user);
+    if (emailExists) {
+      throw new BadRequestException('이미 존재하는 email 입니다.');
+    }
+
+    const nicknameExists = await this.usersRepository.exist({
+      where: {
+        nickname: user.nickname,
+      },
+    });
+
+    if (nicknameExists) {
+      throw new BadRequestException('이미 존재하는 nickname 입니다.');
+    }
+
+    const userObj = this.usersRepository.create({
+      nickname: user.nickname,
+      email: user.email,
+      password: user.password,
+    });
+
+    const newUser = await this.usersRepository.save(userObj);
 
     return newUser;
+  }
+
+  async getUserByEmail(email: string) {
+    return this.usersRepository.findOne({
+      where: {
+        email,
+      },
+    });
   }
 }
